218bc726f9d4dd511c1a62b45e52536e8519d8fc
Lohitha <lohithachowdaryk@gmail.com>
2026-04-02 00:31:31 +0530

    Changed app.py to make db work

--- app/app.py
+++ app/app.py
@@ -195,10 +195,11 @@ def seed_data():
             db.session.add(Category(name=name))
         db.session.commit()
 
+with app.app_context():
+    db.create_all()
+    seed_data()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        seed_data()
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 #check trigger
